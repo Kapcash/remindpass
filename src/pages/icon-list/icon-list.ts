e4182579c07bf0ade5c8a62c83bc40461d9faa8e
Florent Catiau-Tristant <florent.ct@orange.fr>
2018-06-03 17:12:48 +0200

    feat: multiplejs icons gradient background

--- src/pages/icon-list/icon-list.ts
+++ src/pages/icon-list/icon-list.ts
@@ -2,26 +2,29 @@ import { Component } from '@angular/core';
 
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 
+import * as Multiple from '../../../node_modules/multiple.js/multiple.js';
 @Component({
   selector: 'page-icon-list',
   templateUrl: 'icon-list.html'
 })
 export class IconListPage {
-  iconList: Array<string>;
+  iconList: Array<string> = ['logo-apple', 'logo-twitter', 'logo-facebook', 'logo-android',
+  'logo-linkedin', 'logo-google', 'logo-steam', 'logo-windows',
+  'logo-skype', 'logo-snapchat', 'logo-github', 'logo-dropbox',
+  'logo-pinterest', 'logo-youtube', 'logo-whatsapp', 'logo-twitch',
+  'trophy', 'help-circle', 'add', 'search',
+  'attach', 'camera', 'color-palette', 'contact',
+  'football', 'heart', 'mail', 'pin',
+  'settings', 'star', 'mail', 'thumbs-up',
+  'pizza', 'moon', 'musical-notes', 'key'];
 
   constructor (public navCtrl: NavController,
     public navParams: NavParams,
     public viewCtrl: ViewController) {
-      this.iconList = ['logo-apple', 'logo-twitter', 'logo-facebook', 'logo-android',
-        'logo-linkedin', 'logo-google', 'logo-steam', 'logo-windows',
-        'logo-skype', 'logo-slack', 'logo-github', 'logo-dropbox',
-        'logo-pinterest', 'logo-youtube', 'logo-whatsapp', 'logo-twitch',
-        'logo-snapchat', 'help-circle', 'add', 'search',
-        'attach', 'camera', 'color-palette', 'contact',
-        'football', 'heart', 'mail', 'pin',
-        'settings', 'star', 'mail', 'thumbs-up',
-        'pizza', 'moon', 'musical-notes', 'key',
-        'trophy'];
+    const background = new Multiple({
+      selector: '.multiple',
+      background: 'linear-gradient(164deg, #00b4ff, #002c8a)'
+    });
   }
 
   chooseIcon (icon: string) {
